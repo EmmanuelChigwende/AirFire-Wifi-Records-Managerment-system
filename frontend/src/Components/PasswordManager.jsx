755a8f72ee32287b3e4ff79f1bef68b6ddd01a8f
Emmanuel Chigwende <emmaanuelchigwende2@gmail.com>
2025-07-24 09:59:37 +0200

    Delete feature added to password

--- frontend/src/Components/PasswordManager.jsx
+++ frontend/src/Components/PasswordManager.jsx
@@ -19,12 +19,24 @@ const PasswordManager = () => {
         })
     },[])
 
+        const HandleDelete = (id) => {
+         axios.delete(`http://127.0.0.1:5002/password/delete/${id}`)
+        .then(() => {
+        toast.success("Password deleted successfully")
+        setPasswords(prev => prev.filter(item => item._id !== id)) // updates UI immediately
+        })
+        .catch(error => {
+        console.log(error)
+        toast.error("Failed to delete password")
+        })
+    }
+
   return (
     <div className='w-full h-full flex flex-col gap-[10px]'>
         {
             loading ? ("fetching passwords please wait...."): passwords ? (
                 passwords.map(({_id:id,["Name"]:name,["Description"]:details,["Password"]:password})=>(
-                    <div className='w-full h-[200px] rounded-[20px] p-10 gap-6 text-[1.5rem] bg-[#1F305E] text-white grid grid-cols-3 justify-between hover:bg-blue-500'>
+                    <div className='w-full min-h-[200px] rounded-[20px] p-10 gap-6 text-[1.5rem] bg-[#1F305E] text-white grid grid-cols-3 justify-between hover:bg-blue-500'>
                         <p className=' grid grid-rows-2 h-[100px]'>
                             Name <p>{name}</p>
                         </p>
@@ -33,6 +45,9 @@ const PasswordManager = () => {
                         </p>
                         <p className='grid grid-rows-2 h-[100px]'>
                             Password <p>{password}</p>
+                        </p>
+                        <p className=' w-[100px] text-center  rounded-[10px] border-[2px] border-white hover:bg-red-500 '>
+                            <button onClick={() => HandleDelete(id)}>Delete</button>
                         </p>
                     </div>
                 ))
